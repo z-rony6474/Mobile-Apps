e5e5c8785304dd0ccdbfd6e1ca2ab4ad52d15f10
Zahidul Islam Roni <111661615+z-rony6474@users.noreply.github.com>
2024-08-20 00:17:21 +0600

    shoes shop

---
+++ Shoes-Shopping-App-In-Flutter-master/README.md
@@ -0,0 +1,18 @@
+# Shoes-Shopping-App-In-Flutter
+
+> clone this repo
+```
+git clone https://github.com/Sandip-Kanzariya/Shoes-Shopping-App-In-Flutter.git
+```
+> [Follow this to connect firebase ](https://firebase.google.com/docs/flutter/setup?platform=ios)
+
+
+#### Get all dependencies by running this command
+```
+flutter pub get 
+```
+
+
+
+
+
---
+++ Shoes-Shopping-App-In-Flutter-master/lib/pages/cart_page.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/pages/home_page.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_firebase/providers/cart_provider.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = context.watch<CartProvider>().cart;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart'),
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          final cartItem = cart[index];
+
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(cartItem['imageUrl'] as String),
+              radius: 30,
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text(
+                        'Delete Product',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      content: const Text(
+                        'Are you sure you want to remove the product from your cart?',
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text(
+                            'No',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            context
+                                .read<CartProvider>()
+                                .removeProduct(cartItem);
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text(
+                            'Yes',
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+            ),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  cartItem['title'].toString(),
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                Text(
+                  'Price: ${cartItem['price']}',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ],
+            ),
+            subtitle: Text('Size: ${cartItem['size']}'),
+          );
+        },
+      ),
+      bottomNavigationBar: ElevatedButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Your order has been placed.'),
+            ),
+          );
+
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+          );
+          // Clear the cart
+          cart.clear();
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black, // Background color
+          textStyle: TextStyle(
+            color: Colors.white, // Text color
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        child: Text('Check Out'),
+      ),
+    );
+  }
+}
